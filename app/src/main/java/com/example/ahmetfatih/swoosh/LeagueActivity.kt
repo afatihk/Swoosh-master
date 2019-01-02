705eb4e2d11e3071952de234b6108900667bf018
AhmetFatih <afatihkaragoz@hotmail.com>
2019-01-02 23:27:05 +0300

    FİRST CHANGES

--- app/src/main/java/com/example/ahmetfatih/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/ahmetfatih/swoosh/LeagueActivity.kt
@@ -4,17 +4,44 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    fun mensClicked(view : View){
+        WomansButton.isChecked = false
+        Co_edButton.isChecked = false
+        selectedLeague = "Mens"
+    }
+
+    fun womansClicked(view : View){
+        MensButton.isChecked = false
+        Co_edButton.isChecked = false
+        selectedLeague = "Womans"
+    }
+
+    fun coEdClicked(view : View){
+        WomansButton.isChecked = false
+        MensButton.isChecked = false
+        selectedLeague = "Co-Ed"
+    }
+
 
     fun nextClicked(view : View){
-        val skillsIntent = Intent(this, SkillsActivity::class.java)
-        startActivity(skillsIntent)
+        if (selectedLeague != "") {
+            val skillsIntent = Intent(this, SkillsActivity::class.java)
+            skillsIntent.putExtra("lig", selectedLeague)
+            startActivity(skillsIntent)
+        }else{
+            Toast.makeText(applicationContext,"Please Select League", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/ahmetfatih/swoosh/SkillsActivity.kt
+++ app/src/main/java/com/example/ahmetfatih/swoosh/SkillsActivity.kt
@@ -2,12 +2,19 @@ package com.example.ahmetfatih.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class SkillsActivity : AppCompatActivity() {
+class SkillsActivity : BaseActivity() {
+
+    var gelen = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skills)
+
+         gelen = intent.getStringExtra("lig")
+
+        Toast.makeText(applicationContext,gelen,Toast.LENGTH_LONG).show()
 
 
     }
